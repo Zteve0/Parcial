b850d0a88c9773253c0304f747fcf1f5f98fc338
Zteve0 <139638571+Zteve0@users.noreply.github.com>
2023-11-04 10:02:15 -0500

    Parcial Final Segundo intento
    Se redujo sustancialmente las lineas de código y se optimizo ahora ya funcionando.

--- SteveEllisBryan/src/Persona.java
+++ SteveEllisBryan/src/Persona.java
@@ -1,137 +1,118 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
+import java.io.*;
+import java.util.Scanner;
 
-class Persona {
-    private String nombre;
-    private String identificacion;
-
-    public Persona(String nombre, String identificacion) {
-        this.nombre = nombre;
-        this.identificacion = identificacion;
-    }
-
-    public String getIdentificacion() {
-        return identificacion;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-}
-
-class PersonaNatural extends Persona {
-    private String fechaNacimiento;
-
-    public PersonaNatural(String nombre, String identificacion, String fechaNacimiento) {
-        super(nombre, identificacion);
-        this.fechaNacimiento = fechaNacimiento;
-    }
-}
-
-class PersonaJuridica extends Persona {
-    private String razonSocial;
-
-    public PersonaJuridica(String nombre, String identificacion, String razonSocial) {
-        super(nombre, identificacion);
-        this.razonSocial = razonSocial;
-    }
-
-    public String getRazonSocial() {
-        return razonSocial;
-    }
-}
-
-class ONG extends Persona {
-    private String nombreONG;
-
-    public ONG(String nombre, String identificacion, String nombreONG) {
-        super(nombre, identificacion);
-        this.nombreONG = nombreONG;
-    }
-
-    public String getNombreONG() {
-        return nombreONG;
-    }
-}
-
-class Transaccion {
-    private int idTransaccion;
-    private double monto;
-    private String tipoClienteEmisor;
-    private String tipoClienteReceptor;
-    private int horaDelDia;
-    private String ciudadBase;
-
-    public Transaccion(int idTransaccion, double monto, String tipoClienteEmisor, String tipoClienteReceptor, int horaDelDia, String ciudadBase) {
-        this.idTransaccion = idTransaccion;
-        this.monto = monto;
-        this.tipoClienteEmisor = tipoClienteEmisor;
-        this.tipoClienteReceptor = tipoClienteReceptor;
-        this.horaDelDia = horaDelDia;
-        this.ciudadBase = ciudadBase;
-    }
-}
-
-public class BilleteraDigital {
-    private HashMap<String, Persona> personas = new HashMap<String, Persona>();
-
-    public void agregarPersona(String identificacion, Persona persona) {
-        personas.put(identificacion, persona);
-    }
-
-    public void realizarRecargo(String emisor, String receptor, double monto) {
-        try {
-            Persona emisorPersona = personas.get(emisor);
-            Persona receptorPersona = personas.get(receptor);
-
-            if (emisorPersona instanceof PersonaJuridica) {
-                monto *= 2.8;
-            } else if (emisorPersona instanceof ONG) {
-                monto *= 2.0;
-            }
-
-            // Realizar la operación de recarga aquí
-
-        } catch (NullPointerException e) {
-            System.out.println("No se pudo realizar la recarga. Verifique las identificaciones.");
-        }
-    }
-
-    public void realizarTransaccion(int idTransaccion, double monto, String tipoClienteEmisor, String tipoClienteReceptor, int horaDelDia, String ciudadBase, String fecha) {
-        Transaccion transaccion = new Transaccion(idTransaccion, monto, tipoClienteEmisor, tipoClienteReceptor, horaDelDia, ciudadBase);
-
-        // Guardar los datos de la transacción en un archivo .txt correspondiente al día
-        try {
-            String fileName = fecha + ".txt";
-            FileWriter fileWriter = new FileWriter(fileName, true);
-            PrintWriter printWriter = new PrintWriter(fileWriter);
-            printWriter.println("ID=" + idTransaccion + ", Monto=" + monto + ", Emisor=" + tipoClienteEmisor + ", Receptor=" + tipoClienteReceptor + ", Hora=" + horaDelDia + ", Ciudad=" + ciudadBase);
-            printWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+public class Persona {
 
     public static void main(String[] args) {
-        BilleteraDigital billetera = new BilleteraDigital();
+        Scanner scanner = new Scanner(System.in);
+        String[] diaSem = {"lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"};
 
-        PersonaNatural persona1 = new PersonaNatural("Juan", "123456789", "01/01/1990");
-        PersonaJuridica persona2 = new PersonaJuridica("EmpresaX", "987654321", "RazonSocialX");
-        ONG ong1 = new ONG("ONGY", "555555555", "NombreONGY");
+        String[][] data = new String[7][100];
+        double[] maxValorPorDia = new double[7];
 
-        billetera.agregarPersona(persona1.getIdentificacion(), persona1);
-        billetera.agregarPersona(persona2.getIdentificacion(), persona2);
-        billetera.agregarPersona(ong1.getIdentificacion(), ong1);
+        // Leer datos de archivos y calcular los máximos por día
+        for (int i = 0; i < 7; i++) {
+            String fileName = "C:\\Users\\Steve Ellis\\Desktop\\Archivos txt\\" + diaSem[i] + ".txt";
+            double maxDiaValor = 0.0;
 
-        // Realizar transacciones diarias
-        for (int i = 1; i <= 7; i++) {
-            String fecha = "2023-11-0" + i; // Cambia la fecha según corresponda
-            billetera.realizarTransaccion(i, 100000, "PersonaJuridica", "ONG", 12, "CiudadX", fecha);
+            try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+                String line;
+                int a = 0;
+                while ((line = br.readLine()) != null) {
+                    data[i][a] = line;
+                    String[] parts = line.split(";");
+                    if (parts.length >= 5) {
+                        double valor = Double.parseDouble(parts[1]);
+                        if (valor > maxDiaValor) {
+                            maxDiaValor = valor;
+                        }
+                    }
+                    a++;
+                }
+                maxValorPorDia[i] = maxDiaValor;
+            } catch (Exception e) {
+                System.out.println("Error al leer el archivo de " + diaSem[i] + ": " + e.getMessage());
+            }
         }
 
-        // Realizar recargos
-        billetera.realizarRecargo(persona2.getIdentificacion(), ong1.getIdentificacion(), 100000);
+        while (true) {
+            System.out.println("Bienvenido, ¿qué deseas hacer?");
+            System.out.println("1. Día de la semana en que más se movió el dinero");
+            System.out.println("2. Hora del día en que más se movió el dinero");
+            System.out.println("3. Encontrar información por el ID");
+            System.out.println("4. Para ver datos");
+            System.out.println("5. Salir");
+
+            int opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    double maxValor = 0.0;
+                    String diaMaxValor = "";
+
+                    for (int i = 0; i < 7; i++) {
+                        if (maxValorPorDia[i] > maxValor) {
+                            maxValor = maxValorPorDia[i];
+                            diaMaxValor = diaSem[i];
+                        }
+                    }
+
+                    if (!diaMaxValor.isEmpty()) {
+                        System.out.println("El día de la semana con el mayor valor de transacción es " + diaMaxValor);
+                        System.out.println("El valor máximo es: " + maxValor);
+                    }
+                    break;
+                case 2:
+                    findMaxColumn2Value(data);
+                    break;
+                case 3:
+                    System.out.print("Ingrese un ID para buscar información: ");
+                    String idToFind = scanner.nextLine();
+                    // Implementa la lógica para encontrar información por ID
+                    break;
+                case 4:
+                    System.out.println("Datos disponibles:");
+                    for (int i = 0; i < 7; i++) {
+                        System.out.println(diaSem[i] + ": ");
+                        for (int a = 0; a < data[i].length; a++) {
+                            if (data[i][a] != null) {
+                                System.out.println(data[i][a]);
+                            }
+                        }
+                    }
+                    break;
+                case 5:
+                    System.out.println("Saliendo del programa.");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
+            }
+        }
+    }
+
+    private static void findMaxColumn2Value(String[][] data) {
+        double maxColumna2Valor = 0.0;
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                if (data[i][j] != null) {
+                    String[] parts = data[i][j].split(";");
+                    if (parts.length >= 5) {
+                        try {
+                            double columna2Valor = Double.parseDouble(parts[4]);
+                            if (columna2Valor > maxColumna2Valor) {
+                                maxColumna2Valor = columna2Valor;
+                            }
+                        } catch (NumberFormatException e) {
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println("La hora es: " + maxColumna2Valor);
     }
 }
